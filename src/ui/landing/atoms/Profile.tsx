0c4352d70b9e23ed9eb469ef86bca5fffccb7e10
aaryae <aaryaed@gmail.com>
2025-09-24 23:39:52 +0545

    chore: ts config path added

--- src/ui/landing/atoms/Profile.tsx
+++ src/ui/landing/atoms/Profile.tsx
@@ -1,13 +1,11 @@
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Profile = ({value}:{value?:string}) => {
+const Profile = ({ value }: { value?: string }) => {
   return (
     <div className='flex flex-col '>
-      <FontAwesomeIcon icon={faUser} size='1x' style={{ color: 'black', margin:'auto' }} />
-      <div className='text-sm'>
-      {value??"Guest"}
-      </div>
+      <FontAwesomeIcon icon={faUser} size='1x' style={{ color: 'black', margin: 'auto' }} />
+      <div className='text-sm'>{value ?? 'Guest'}</div>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,9 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    tailwindcss(),
-    tsconfigPaths(), 
-
-  ],
+  plugins: [tailwindcss(), tsconfigPaths()],
 })

3f3aecff7f6dc2aec0df00e1a541c57c6f245804
aaryae <aaryaed@gmail.com>
2025-12-09 21:24:25 +0545

    feat: qr added

--- src/ui/user/pages/MyQr.tsx
+++ src/ui/user/pages/MyQr.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import QRCode from 'react-qr-code'
 
 const MyQr = () => {
   return (
     <div>
-      This is my qr
+      <div style={{ height: 'auto', margin: '0 auto', maxWidth: 300, width: '100%' }}>
+        <QRCode
+          size={256}
+          style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
+          value='this is my data'
+          viewBox={`0 0 256 256`}
+        />
+      </div>
+      <h1 className='text-center py-6'>Scan to get the account number</h1>
     </div>
   )
 }
